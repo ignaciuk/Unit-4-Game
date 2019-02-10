684ec32cd551513ff51b38bbb4574ac3885d59bb
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-02-10 09:03:19 -0500

    game logic done and styling mostly finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    var currentScore;
+    var targetScore;
+    var winTotal = 0;
+    var lossTotal = 0;
+    var gamesPlayed = -1;
+    var buttonOneValue;
+    var buttonTwoValue;
+    var buttonThreeValue;
+    var buttonFourValue;
+    $("#total-wins").text(winTotal);
+    $("#total-losses").text(lossTotal);
+    $("#games-played").text(gamesPlayed);
+
+
+        function gameReStart() {
+            currentScore = 0;
+            $("#current-score").text(currentScore);
+            targetScore = 0;
+            buttonOneValue = 0;
+            buttonTwoValue = 0;
+            buttonThreeValue = 0;
+            buttonFourValue = 0;
+            gamesPlayed ++;
+            $("#games-played").text(gamesPlayed);
+            gameStart();
+        }
+
+        function gameStart() {
+            targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+            $("#target-score").text(targetScore);
+            buttonOneValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+            buttonTwoValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+            buttonThreeValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+            buttonFourValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+
+            $("#red-crystal-skull-btn").unbind().on("click", function() { 
+                currentScore += buttonOneValue;
+                $("#current-score").text(currentScore);
+                checkScore();
+            });
+
+            $("#blue-crystal-skull-btn").unbind().on("click", function() { 
+                currentScore += buttonTwoValue;
+                $("#current-score").text(currentScore);
+                checkScore();
+            });
+
+            $("#green-crystal-skull-btn").unbind().on("click", function() { 
+                currentScore += buttonThreeValue;
+                $("#current-score").text(currentScore);
+                checkScore();
+            });
+
+            $("#magenta-crystal-skull-btn").unbind().on("click", function() { 
+                currentScore += buttonFourValue;
+                $("#current-score").text(currentScore);
+                checkScore();
+            });
+
+
+            function checkScore() {
+                if (currentScore === targetScore) {
+                    alert("Congratulations! You've won!");
+                    winTotal++;
+                    $("#total-wins").text(winTotal);
+                    gameReStart();
+                } else if (currentScore > targetScore) {
+                    alert("Sorry, you've lost");
+                    lossTotal++;
+                    $("#total-losses").text(lossTotal);
+                    gameReStart();
+                    }
+            }
+        }
+
+    gameReStart();
+
+
+});
